b50098923457acc1fd7c6ce56b3714e124aa5173
Baptiste DUPUCH <baptiste.dupuch@gmail.com>
2012-09-05 22:44:07 +0200

    Travail sur l'ajout d'un produit à un shop/a finir

---
+++ src/PiggyBox/ShopBundle/Form/ProductType.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace PiggyBox\ShopBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+class ProductType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('name')
+            ->add('description')
+            ->add('price_kg')
+            ->add('active')
+            ->add('image_path')
+            ->add('promo_active')
+            ->add('promo_price')
+            ->add('promo_expire_date')
+            ->add('promo_percentage')
+        ;
+    }
+
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'PiggyBox\ShopBundle\Entity\Product'
+        ));
+    }
+
+    public function getName()
+    {
+        return 'piggybox_shopbundle_producttype';
+    }
+}
