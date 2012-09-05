b47ea81d4079a79ab232a3e09aecb23176792a7b
Baptiste DUPUCH <baptiste.dupuch@gmail.com>
2012-09-05 08:32:13 +0200

    Création des utilisateurs et des managers de magasin

---
+++ src/PiggyBox/ManagerBundle/Entity/Manager.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace PiggyBox\ManagerBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * PiggyBox\ManagerBundle\Entity\Manager
+ *
+ * @ORM\Table()
+ * @ORM\Entity(repositoryClass="PiggyBox\ManagerBundle\Entity\ManagerRepository")
+ */
+class Manager
+{
+    /**
+     * @var integer $id
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+}
---
+++ src/PiggyBox/UserBundle/Entity/User.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace PiggyBox\UserBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * PiggyBox\UserBundle\Entity\User
+ *
+ * @ORM\Table()
+ * @ORM\Entity(repositoryClass="PiggyBox\UserBundle\Entity\UserRepository")
+ */
+class User
+{
+    /**
+     * @var integer $id
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+}
