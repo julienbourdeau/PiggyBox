9c492fe2e6f774e769667dab890d5bc806639565
Baptiste DUPUCH <baptiste.dupuch@gmail.com>
2012-09-10 18:43:47 +0200

    Modification de la fonction dajout au panier pour qu'elle puisse prendre en compte le panier multi magasin

--- src/PiggyBox/OrderBundle/Controller/OrderController.php
+++ src/PiggyBox/OrderBundle/Controller/OrderController.php
@@ -12,6 +12,7 @@ use PiggyBox\OrderBundle\Form\OrderType;
 use PiggyBox\OrderBundle\Entity\OrderDetail;
 use PiggyBox\ShopBundle\Entity\Product;
 use Symfony\Component\HttpFoundation\RedirectResponse;
+use PiggyBox\OrderBundle\Entity\Cart;
 
 /**
  * Order controller.
@@ -24,23 +25,27 @@ class OrderController extends Controller
     /**
      * Crée ou récupère un nouvel Order grâce à la session du l'utilisateur 
      *
-     * @Route("/ajouter/produit", name="order_add_product")
+     * @Route("/ajouter/produit", name="cart_add_product")
      */
     public function addProductAction(Request $req)
     {
         //NOTE: Get the CartProvider that handle the creation/retreive of the cart and session
-        $order = $this->get('piggy_box_order.provider')->getOrder();
+        $cart = $this->get('piggy_box_cart.provider')->getCart();
 
-        //NOTE: Création d'un nouvel OrderDetail à l'Order
-		$order_detail = new OrderDetail();
-		$order_detail->setOrder($order);
-//		$order->addOrderDetail($order_detail);
-
-		//Ajout du produit à l'OrderDetail
         $em = $this->getDoctrine()->getManager();
 		$product = $em->getRepository('PiggyBoxShopBundle:Product')->find($req->get('id'));
+		$order = $em->getRepository('PiggyBoxOrderBundle:Cart')->getOrderByShop($product->getShop()->getId());
+		$order_detail = new OrderDetail();
+		
+		if(null == $order){
+			$order = new Order();
+		}
+
+		//Ajout du produit à l'OrderDetail
+		$order_detail->setOrder($order);	
         $order_detail->setProduct($product);
 		$order_detail->setPrice($em->getRepository('PiggyBoxShopBundle:Price')->find($req->get('price_id')));
+		$order->setShop($product->getShop());
 
         $em->persist($order_detail);
         $em->persist($order);
@@ -76,20 +81,20 @@ class OrderController extends Controller
     }
 
     /**
-     * Validate order 
+     * Validate cart 
      *
      * @Route("/validation", name="validate_order")
      */
     public function validateOrderAction(Request $req)
     {
 		//NOTE: Get the CartProvider that handle the creation/retreive of the cart and session
-        $order = $this->get('piggy_box_order.provider')->getOrder();
-		$order->setStatus("sent");
-		$order->setUser($this->get('security.context')->getToken()->getUser());
+        $cart = $this->get('piggy_box_cart.provider')->getOrder();
+		$cart->setStatus("sent");
+		$cart->setUser($this->get('security.context')->getToken()->getUser());
 		//Ajout du produit à l'OrderDetail
         $em = $this->getDoctrine()->getManager();
 
-        $em->persist($order);
+        $em->persist($cart);
         $em->flush();
 
         //NOTE: Set a flash message to share the success
