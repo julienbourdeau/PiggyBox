fb3c14b826758fbd3b2936bae6e203c0956983c0
Julien Bourdeau <julien@sigerr.org>
2012-09-20 11:56:06 +0200

    changed my shit

---
+++ src/PiggyBox/OrderBundle/Storage/SessionCartStorage.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace PiggyBox\OrderBundle\Storage;
+
+use PiggyBox\OrderBundle\Entity\Cart;
+use Symfony\Component\HttpFoundation\Session\Session;
+
+/**
+ * Stores current cart id inside the user session.
+ *
+ */
+class SessionCartStorage
+{
+    /**
+     * Session.
+     *
+     * @var Session
+     */
+    protected $session;
+
+    /**
+     * Constructor.
+     *
+     * @param Session $session
+     */
+    public function __construct(Session $session)
+    {
+        $this->session = $session;
+    }
+
+    public function getCurrentCartIdentifier()
+    {
+        return $this->session->get('_piggybox.cart-id');
+    }
+
+    public function setCurrentCartIdentifier(Cart $cart)
+    {
+        $this->session->set('_piggybox.cart-id', $cart->getId());
+    }
+
+    public function resetCurrentCartIdentifier()
+    {
+        $this->session->remove('_piggybox.cart-id');
+    }
+}
+
+
---
+++ src/PiggyBox/OrderBundle/Twig/CartExtension.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace PiggyBox\OrderBundle\Twig;
+
+use Twig_Extension;
+use PiggyBox\OrderBundle\Provider\CartProvider;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
+class CartExtension extends \Twig_Extension
+{
+    /**
+     * Cart provider.
+     *
+     * @var CartProvider
+     */
+    private $container;
+
+    /**
+     * Constructor.
+     *
+     * @param ContainerInterface $container
+     */
+    public function __construct(ContainerInterface $container)
+    {
+        $this->container = $container;
+    }
+
+    /**
+     * Functions declaration
+    */
+    public function getFunctions()
+    {
+        return array(
+            'piggybox_orders_get' => new \Twig_Function_Method($this,'getCurrentOrders'),
+            );
+    }
+
+    /**
+     * Returns a list of tests to add to the existing list.
+     *
+     * @return cart
+     */
+    public function getCurrentOrders()
+    {
+       $em = $this->container->get('doctrine.orm.default_entity_manager');
+       return  $this->container->get('piggy_box_cart.provider')->getCart()->getOrders();
+    }
+
+    public function getName()
+    {
+        return 'cart_extension';
+    }
+}
+
+
