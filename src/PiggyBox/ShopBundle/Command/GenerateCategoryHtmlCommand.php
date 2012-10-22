f6582aac5bd35f9c14a59e034d38e239712e5020
Baptiste DUPUCH <baptiste.dupuch@gmail.com>
2012-10-22 15:26:48 +0200

    Hotfix de command de génération de catégories

--- src/PiggyBox/ShopBundle/Command/GenerateCategoryHtmlCommand.php
+++ src/PiggyBox/ShopBundle/Command/GenerateCategoryHtmlCommand.php
@@ -45,15 +45,16 @@ class GenerateCategoryHtmlCommand extends ContainerAwareCommand
 						$category = $category->getParent();
 					}
 				
-					$html.='<li class=""><a href="$this->getContainer()->get(\'router\')->generate(\'user_show_shop\',array(\'slug\' => $shop->getSlug(), \'category_title\' => $category->getTitle())).'" class="category-item'.$category->getTitle().'">'.$category->getTitle().'</a></li>';
+					$html.='<li class=""><a href="'.$this->getContainer()->get('router')->generate('user_show_shop',array('slug' => $shop->getSlug(), 'category_title' => $category->getTitle())).'" class="category-item'.$category->getTitle().'">'.$category->getTitle().'</a></li>';
+
 					$children_categories = $category->getChildren();
 					$html.='<ul class="nav nav-list">';
 
 					foreach ($children_categories as $children_category) {
 						if(in_array($children_category, $categories) and !$categories_buffer->contains($children_category))
 						{
-								$html.='<li class=""><a href="$this->getContainer()->get(\'router\')->generate(\'user_show_shop\',array(\'slug\' => $shop->getSlug(), \'category_title\' => $children_category->getTitle())).'" class="category-item '.$children_category->getTitle().' ">'.$children_category->getTitle().'</a></li>';
-								$categories_buffer->add($children_category);
+							$html.='<li class=""><a href="'.$this->getContainer()->get('router')->generate('user_show_shop',array('slug' => $shop->getSlug(), 'category_title' => $children_category->getTitle())).'" class="category-item '.$children_category->getTitle().' ">'.$children_category->getTitle().'</a></li>';
+							$categories_buffer->add($children_category);
 						}
 					}
 					$categories_buffer->add($category);
