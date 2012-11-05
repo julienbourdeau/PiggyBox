2fd13f25ed0164654440bacd34ae36d6fbea1492
Baptiste DUPUCH <baptiste.dupuch@gmail.com>
2012-11-05 16:38:00 +0100

    php-cs-fixer + desactivation du responsive pour tout le site

--- src/PiggyBox/ShopBundle/Command/GenerateCategoryHtmlCommand.php
+++ src/PiggyBox/ShopBundle/Command/GenerateCategoryHtmlCommand.php
@@ -3,18 +3,14 @@
 namespace PiggyBox\ShopBundle\Command;
 
 use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
-use Symfony\Bridge\Doctrine\RegistryInterface;
 use Symfony\Component\Console\Input\InputInterface;
-use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
-use Symfony\Component\HttpKernel\KernelInterface;
-use Symfony\Component\Finder\Finder;
 use PiggyBox\ShopBundle\Entity\Shop;
 use PiggyBox\ShopBundle\Entity\Category;
-	
+
 class GenerateCategoryHtmlCommand extends ContainerAwareCommand
 {
-	
+
     protected function configure()
     {
         $this
@@ -24,47 +20,44 @@ class GenerateCategoryHtmlCommand extends ContainerAwareCommand
 
     /**
      * {@inheritdoc}
-     */     
+     */
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-		$em = $this->getContainer()->get('doctrine.orm.default_entity_manager');
-		$shops = $em->getRepository('PiggyBoxShopBundle:Shop')->findAll();
-		
-		foreach ($shops as $shop) {
-			$categories = $em->createQuery('SELECT DISTINCT c, p FROM PiggyBoxShopBundle:Category c JOIN c.products p  WHERE p.shop=:id')
-									  ->setParameter('id', $shop->getId())	
-									  ->getResult();
-		
-			$categories_buffer = new \Doctrine\Common\Collections\ArrayCollection();
-			$html = '';
+        $em = $this->getContainer()->get('doctrine.orm.default_entity_manager');
+        $shops = $em->getRepository('PiggyBoxShopBundle:Shop')->findAll();
 
-			foreach($categories as $category){
-				if(!$categories_buffer->contains($category))
-				{
-					if($category->getLevel() !=0){
-						$category = $category->getParent();
-					}
-				
-					$html.='<li id="'.$category->getSlug().'" class=""><a href="'.$this->getContainer()->get('router')->generate('user_show_shop',array('slug' => $shop->getSlug(), 'category_title' => $category->getTitle())).'" class="category-item '.$category->getTitle().'">'.$category->getTitle().'</a></li>';
+        foreach ($shops as $shop) {
+            $categories = $em->createQuery('SELECT DISTINCT c, p FROM PiggyBoxShopBundle:Category c JOIN c.products p  WHERE p.shop=:id')
+                                      ->setParameter('id', $shop->getId())
+                                      ->getResult();
 
-					$children_categories = $category->getChildren();
-					$html.='<ul class="nav nav-list">';
+            $categories_buffer = new \Doctrine\Common\Collections\ArrayCollection();
+            $html = '';
 
-					foreach ($children_categories as $children_category) {
-						if(in_array($children_category, $categories) and !$categories_buffer->contains($children_category))
-						{
-							$html.='<li id="'.$children_category->getSlug().'" class=""><a href="'.$this->getContainer()->get('router')->generate('user_show_shop',array('slug' => $shop->getSlug(), 'category_title' => $children_category->getTitle())).'" class="category-item '.$children_category->getTitle().' ">'.$children_category->getTitle().'</a></li>';
-							$categories_buffer->add($children_category);
-						}
-					}
-					$categories_buffer->add($category);
-					$html.='</ul>';	
-				}		
-			}
-			$shop->setCategoryHtml($html);
-			$em->persist($shop);
-			$em->flush();
-		}
+            foreach ($categories as $category) {
+                if (!$categories_buffer->contains($category)) {
+                    if ($category->getLevel() !=0) {
+                        $category = $category->getParent();
+                    }
+
+                    $html.='<li id="'.$category->getSlug().'" class=""><a href="'.$this->getContainer()->get('router')->generate('user_show_shop',array('slug' => $shop->getSlug(), 'category_title' => $category->getTitle())).'" class="category-item '.$category->getTitle().'">'.$category->getTitle().'</a></li>';
+
+                    $children_categories = $category->getChildren();
+                    $html.='<ul class="nav nav-list">';
+
+                    foreach ($children_categories as $children_category) {
+                        if (in_array($children_category, $categories) and !$categories_buffer->contains($children_category)) {
+                            $html.='<li id="'.$children_category->getSlug().'" class=""><a href="'.$this->getContainer()->get('router')->generate('user_show_shop',array('slug' => $shop->getSlug(), 'category_title' => $children_category->getTitle())).'" class="category-item '.$children_category->getTitle().' ">'.$children_category->getTitle().'</a></li>';
+                            $categories_buffer->add($children_category);
+                        }
+                    }
+                    $categories_buffer->add($category);
+                    $html.='</ul>';
+                }
+            }
+            $shop->setCategoryHtml($html);
+            $em->persist($shop);
+            $em->flush();
+        }
     }
 }
-
