0decf2713d095aeb437a3a2e085c6c72c53f015c
Julien Bourdeau <julien@sigerr.org>
2012-12-17 17:09:37 +0100

    added some style for Menus

--- src/PiggyBox/UserBundle/Resources/public/js/main.js
+++ src/PiggyBox/UserBundle/Resources/public/js/main.js
@@ -4,3 +4,17 @@ $('a[rel=tooltip]').tooltip();
 $('.carousel').carousel({
   interval: 3000
 });
+
+$('input[type="radio"]').change(function() {
+   
+   $('input[type="radio"]').each(function() {
+	   if($(this).is(':checked'))  {
+	  		var theID = $(this).attr('id');
+	  		$('label[for="'+ theID +'"]').children('.product-item').addClass('selectedItem');
+	    } else {
+	  		var theID = $(this).attr('id');
+	  		$('label[for="'+ theID +'"]').children('.product-item').removeClass('selectedItem');
+	    }
+   });
+
+});
