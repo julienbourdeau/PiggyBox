7bce7be6e7d8c8b2f6855c3563804c2c9c41f210
Baptiste DUPUCH <baptiste.dupuch@gmail.com>
2013-01-22 09:00:46 +0100

    Création de l'entité discount qui va générer (ou pas) des discount pour les produits souhaités.

---
+++ src/PiggyBox/ShopBundle/Entity/Discount.php
@@ -0,0 +1,124 @@
+<?php
+
+namespace PiggyBox\ShopBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Discount
+ *
+ * @ORM\Table()
+ * @ORM\Entity
+ */
+class Discount
+{
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="discountName", type="string", length=255)
+     */
+    private $discountName;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="discountDescription", type="string", length=255)
+     */
+    private $discountDescription;
+
+    /**
+     * @var float
+     *
+     * @ORM\Column(name="discountPrice", type="float")
+     */
+    private $discountPrice;
+
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set discountName
+     *
+     * @param string $discountName
+     * @return Discount
+     */
+    public function setDiscountName($discountName)
+    {
+        $this->discountName = $discountName;
+    
+        return $this;
+    }
+
+    /**
+     * Get discountName
+     *
+     * @return string 
+     */
+    public function getDiscountName()
+    {
+        return $this->discountName;
+    }
+
+    /**
+     * Set discountDescription
+     *
+     * @param string $discountDescription
+     * @return Discount
+     */
+    public function setDiscountDescription($discountDescription)
+    {
+        $this->discountDescription = $discountDescription;
+    
+        return $this;
+    }
+
+    /**
+     * Get discountDescription
+     *
+     * @return string 
+     */
+    public function getDiscountDescription()
+    {
+        return $this->discountDescription;
+    }
+
+    /**
+     * Set discountPrice
+     *
+     * @param float $discountPrice
+     * @return Discount
+     */
+    public function setDiscountPrice($discountPrice)
+    {
+        $this->discountPrice = $discountPrice;
+    
+        return $this;
+    }
+
+    /**
+     * Get discountPrice
+     *
+     * @return float 
+     */
+    public function getDiscountPrice()
+    {
+        return $this->discountPrice;
+    }
+}
