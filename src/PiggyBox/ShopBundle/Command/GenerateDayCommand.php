da6b9d9cd49e6132eac39465334fbbc9de0e4347
Baptiste DUPUCH <baptiste.dupuch@gmail.com>
2013-01-23 22:27:44 +0100

    Command enfin finit...

--- src/PiggyBox/ShopBundle/Command/GenerateDayCommand.php
+++ src/PiggyBox/ShopBundle/Command/GenerateDayCommand.php
@@ -39,89 +39,58 @@ class GenerateDayCommand extends ContainerAwareCommand
         $shopSlug = $input->getArgument('shopSlug');
         $shop = $em->getRepository('PiggyBoxShopBundle:Shop')->findOneBySlug($shopSlug);
 
+		$openingDays = $shop->getOpeningDays();
+		foreach ($openingDays as $day) {
+			$shop->removeOpeningDay($day);
+			$em->remove($day);
+		}
+		$em->persist($shop);
+		$em->flush();
+
 		$dialog = $this->getHelperSet()->get('dialog');
-		
 
+		for ($i = 1; $i < 8; $i++) {
+			$monday = new Day();
+			$monday->setDayOfTheWeek($i);
+			$opened = $dialog->ask(
+				$output,
+				'Le magasin est-il ouvert le jour '.$i.'? (y or n (default y))',
+				true
+			);
+			if ($opened === 'y') {
+				$opened = true;			
+			}
+			if ($opened === 'n') {
+				$opened = false;
+			}
+			$monday->setOpen($opened); 
+			$fromTimeMorning = $dialog->ask(
+				$output,
+				'A partir de quelle heure ouvre t-il (e.g 06:30 null si ENTER)',
+				null
+			);		
+			$monday->setFromTimeMorning(new \DateTime($fromTimeMorning));
+			$toTimeMorning = $dialog->ask(
+				$output,
+				'A partir de quelle heure ferme t-il le matin (e.g 12:30)',
+				null
+			);				
+			$monday->setToTimeMorning(new \DateTime($toTimeMorning));
+			$setFromTimeAfternoon = $dialog->ask(
+				$output,
+				'A partir de quelle heure ouvre t-il l\'après-midi ? (e.g 13:30)',
+				null
+			);		
+			$monday->setFromTimeAfternoon($setFromTimeAfternoon);
+			$setToTimeAfternoon = $dialog->ask(
+				$output,
+				'A partir de quelle heure ferme t-il le soir? (e.g 21:30)',
+				null
+			);				
+			$monday->setToTimeAfternoon($setToTimeAfternoon);
 
-        $monday = new Day();
-        $monday->setDayOfTheWeek(1);
-		$opened = $dialog->ask(
-    		$output,
-    		'Le magasin est-il ouvert le Lundi? (y or n (default y))',
-    		true
-		);
-		if ($opened == 'y') {
-			$opened = true;			
+        $shop->addOpeningDay($monday);			
 		}
-		if ($opened == 'n') {
-			$opened = false;
-		}
-		$monday->setOpen($opened); 
-		$fromTimeMorning = $dialog->ask(
-    		$output,
-    		'A partir de quelle heure ouvre t-il (e.g 06:30)',
-			null
-		);		
-		$monday->setFromTimeMorning(new \DateTime($fromTimeMorning));
-		$monday->setToTimeMorning(new \DateTime('21:00'));
-		//$monday->setFromTimeAfternoon();
-		//$monday->setToTimeAfternoon();
-			
-		$tuesday = new Day();
-        $tuesday->setDayOfTheWeek(2);
-		$tuesday->setOpen(true); 
-		$tuesday->setFromTimeMorning(new \DateTime('06:30'));
-		$tuesday->setToTimeMorning(new \DateTime('21:00'));
-	//	$tuesday->setFromTimeAfternoon();
-	//	$tuesday->setToTimeAfternoon();
-			
-		$wednesday = new Day();
-        $wednesday->setDayOfTheWeek(3);
-		$wednesday->setOpen(false); 
-		$wednesday->setFromTimeMorning(new \DateTime('06:30'));
-		$wednesday->setToTimeMorning(new \DateTime('21:00'));
-	//	$wednesday->setFromTimeAfternoon();
-	//	$wednesday->setToTimeAfternoon();
-			
-		$thursday = new Day();
-        $thursday->setDayOfTheWeek(4);
-		$thursday->setOpen(true); 
-		$thursday->setFromTimeMorning(new \DateTime('06:30'));
-		$thursday->setToTimeMorning(new \DateTime('21:00'));
-	//	$thursday->setFromTimeAfternoon();
-	//	$thursday->setToTimeAfternoon();
-			
-		$friday = new Day();
-        $friday->setDayOfTheWeek(5);
-		$friday->setOpen(true); 
-		$friday->setFromTimeMorning(new \DateTime('06:30'));
-		$friday->setToTimeMorning(new \DateTime('21:00'));
-	//	$friday->setFromTimeAfternoon();
-	//	$friday->setToTimeAfternoon();
-			
-		$saturday = new Day();
-        $saturday->setDayOfTheWeek(6);
-		$saturday->setOpen(true); 
-		$saturday->setFromTimeMorning(new \DateTime('06:30'));
-		$saturday->setToTimeMorning(new \DateTime('21:00'));
-	//	$saturday->setFromTimeAfternoon();
-	//	$saturday->setToTimeAfternoon();
-
-        $sunday = new Day();
-        $sunday->setDayOfTheWeek(7);
-		$sunday->setOpen(true); 
-		$sunday->setFromTimeMorning(new \DateTime('06:30'));
-		$sunday->setToTimeMorning(new \DateTime('21:00'));
-	//	$sunday->setFromTimeAfternoon();
-	//	$sunday->setToTimeAfternoon();
-
-        $shop->addOpeningDay($monday);
-        $shop->addOpeningDay($tuesday);
-        $shop->addOpeningDay($wednesday);
-        $shop->addOpeningDay($thursday);
-        $shop->addOpeningDay($friday);
-        $shop->addOpeningDay($saturday);
-        $shop->addOpeningDay($sunday);
 
         $em->persist($shop);
         $em->flush();
