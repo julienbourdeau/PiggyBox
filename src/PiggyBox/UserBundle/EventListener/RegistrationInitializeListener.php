06ec204cd0769f4dcb25b1e394326db2308d5ea2
Baptiste Dupuch <baptiste.dupuch@gmail.com>
2013-03-11 12:26:37 +0100

    Ajout de la bonne redirection après inscription permettant à l'utilisateur d'être ramené sur sa commande lorsqu'il s'enregistre pour la première fois

--- src/PiggyBox/UserBundle/EventListener/RegistrationInitializeListener.php
+++ src/PiggyBox/UserBundle/EventListener/RegistrationInitializeListener.php
@@ -11,7 +11,6 @@ use Symfony\Component\EventDispatcher\EventSubscriberInterface;
  */
 class RegistrationInitializeListener implements EventSubscriberInterface
 {
-
     /**
      * {@inheritDoc}
      */
