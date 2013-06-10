775c96a5a22a0bf989b5f21d0d1fa79abfcb5d3c
Julien Bourdeau <julien@sigerr.org>
2013-06-10 13:47:27 +0200

    added file_exists() function

---
+++ src/PiggyBox/UserBundle/Twig/UserExtension.php
@@ -0,0 +1,32 @@
+<?php 
+
+namespace PiggyBox\UserBundle\Twig;
+
+use \Twig_Filter_Function;
+use \Twig_Filter_Method;
+
+class UserExtension extends \Twig_Extension
+{
+
+    /**
+     * Return the functions registered as twig extensions
+     * 
+     * @return array
+     */
+    public function getFunctions()
+    {
+        return array(
+            'file_exists' => new \Twig_Function_Function('file_exists'),
+        );
+    }
+    /*
+    public function fileExistsFunction($filename)
+    {
+        return file_exists($filename);
+    }
+*/
+    public function getName()
+    {
+        return 'twig_extension';
+    }
+}
